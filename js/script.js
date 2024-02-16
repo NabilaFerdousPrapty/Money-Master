5c00505d45395b85b6d0618e64f842571798a2b4
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-02-16 06:38:42 +0600

    almost done

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// console.log('connected');
+ const totalExpanse=0;
+function calculateExpanse() {
+    
+    const income=getElementValueById("income-value");
+    // console.log(income);
+    const food=getElementValueById("food-value");
+    // console.log(food);
+    const rent=getElementValueById('rent-value');
+    // console.log(rent);
+    const cloths=getElementValueById('cloths-value');
+    // console.log(cloths);
+    const totalExpanse=food+rent+cloths;
+    // console.log(totalExpanse);
+    const remaining=income-totalExpanse;
+    document.getElementById('total-expanse').innerText=totalExpanse;
+    document.getElementById('balance').innerText=income;
+    document.getElementById('remaining').innerText=remaining;
+    return remaining;
+    
+
+}
+function calculateSavings() {
+    const savingPercentage=getElementValueById('saving-percentage');
+    // console.log(savingPercentage);
+    const remaining=calculateExpanse();
+    const saving=remaining-remaining*(savingPercentage/100);
+   const remain=remaining-saving;
+    document.getElementById('saving').innerText=saving;
+    document.getElementById('remaining').innerText=remain;
+
+}
+
+
+function getElementValueById(elementId) {
+    const element=document.getElementById(elementId);
+    // console.log(element);
+    const elementText=element.value ;
+    // console.log(elementText);
+    const elementValue=parseInt(elementText);
+    // console.log(elementValue);
+    return elementValue;
+}
